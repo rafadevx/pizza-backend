18a6df78ee1784a2c1dafd6eec1756b571d88741
Rafael <rafa.comp@gmail.com>
2019-06-10 17:29:12 -0300

    criacao do controller de user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,6 +2,12 @@ const { User } = require('../models')
 
 class UserController {
   async store (req, res) {
+    const { email } = req.body
+
+    if (await User.findOne({ email })) {
+      return res.status(400).json({ error: 'User already exists' })
+    }
+
     const user = await User.create(req.body)
 
     return res.json(user)
